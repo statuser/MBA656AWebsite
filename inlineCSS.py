b5a0442a90897f133d89cf476a8afdbfb602db8c
John Howell <john.r.howell@gmail.com>
2020-08-31 15:40:19 -0600

    Initial Commit

---
+++ inlineCSS.py
@@ -0,0 +1,12 @@
+from inlinestyler.utils import inline_css
+import os, sys
+
+
+htmlFile = open(sys.argv[-1], "r")
+message_inline_css = inline_css(htmlFile.read())
+htmlFile.close()
+
+outputHtml = "inlined-" + sys.argv[-1]
+outputFile = open(outputHtml, "a")
+outputFile.write(message_inline_css)
+outputFile.close()
---
+++ tenzi.py
@@ -0,0 +1,18 @@
+import random
+
+def roll(dice_list, keep_number=None):
+    for index, die in enumerate(dice_list):
+        if keep_number is None or die != keep_number:
+            dice_list[index] = random.choice(range(1,7))
+    return dice_list
+
+dice = [None]*10
+input("Press enter to start")
+dice = roll(dice)
+target_number = max(dice, key=dice.count) # Automatically resolves ties
+dice_to_reroll = 10 - dice.count(target_number)
+while dice_to_reroll > 0:
+    dice = roll(dice, target_number)
+    print(dice)
+    dice_to_reroll = 10 - dice.count(target_number)
+print("TENZI")
