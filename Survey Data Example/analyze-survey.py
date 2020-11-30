812e2546677987dce63fdedea360c01654dfd408
John Howell <john.r.howell@gmail.com>
2020-11-30 09:05:12 -0700

    Update Formatting

--- Survey Data Example/analyze-survey.py
+++ Survey Data Example/analyze-survey.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 # Read in data
-data = pd.read_csv('./Survey Data Example/DigitalCamera_030413.csv', parse_dates=[1,2])
+data = pd.read_csv('./Survey Data Example/DigitalCamera_030413.csv', parse_dates=[1, 2])
 print(data.head())
 print(data.columns)
 print(data.dtypes)
@@ -21,7 +21,7 @@ max(data["Age"])
 
 # 25 - 80 What groups do we want?
 labels = ["18 - 35", "36 - 50", "51 - 65", "65+"]
-data["AgeGroups"] = pd.cut(data["Age"], bins = [18, 35, 50, 65, 100], labels = labels)
+data["AgeGroups"] = pd.cut(data["Age"], bins=[18, 35, 50, 65, 100], labels=labels)
 
 # We have 6 education categories - We only want 3 - No College (1-2), Undergraduate (3-4), Advanced Degree (5-6)
 data["Education"] = data["D6"]
@@ -30,14 +30,14 @@ data.loc[data["Education"] == 3, "Education"] = 2
 data.loc[data["Education"] == 4, "Education"] = 2
 data.loc[data["Education"] == 5, "Education"] = 3
 data.loc[data["Education"] == 6, "Education"] = 3
-data["Education"] = data["Education"].astype('category').cat.rename_categories(['No College', 'Undergraduate', 'Advanced Degree'])
+data["Education"] = data["Education"].astype('category').cat.rename_categories(['No College',
+                                                                                'Undergraduate',
+                                                                                'Advanced Degree'])
 
 data["Education"].value_counts().plot()
 
 
-# Continue for other quesitons.  
-
-
+# Continue for other quesitons.
 
 # Clean Data
 
@@ -46,7 +46,7 @@ data["Education"].value_counts().plot()
 data = data[data["T1_7"] == 0]
 
 # Check for straight lining
-# This looks across multiple questions to see if the variance is 0.0.  It the variance is 0.0 then the respondnet 
+# This looks across multiple questions to see if the variance is 0.0.  It the variance is 0.0 then the respondents
 # answered the same for 16 straight questions
 data = data[data.loc[:, ["DC_{col}".format(col=i) for i in range(1, 17)]].var(axis=1) != 0.0]
 
@@ -60,5 +60,3 @@ pd.crosstab(data["Education"], data["Gender"])
 
 # Plot stuff
 data["Gender"].value_counts().plot()
-
-# Demonstrate Statistical Testing?
